64895a56835b7c04fc8483d2196ca3deec60ff28
Hisham-Mohsen <mohsenhisham2@gmail.com>
2022-05-28 01:55:19 +0300

    Creating createRestaurant method in Restaurant class

--- Restaurant.php
+++ Restaurant.php
@@ -5,8 +5,15 @@ class Restaurant extends Database
 {
     private $table = "restaurants";
 
-    public function createRestaurant()
+    public function createRestaurant($rest_name, $rest_desc, $rest_pic)
     {
+        $query_string = "INSERT INTO $this->table 
+                        (rest_name, rest_desc, rest_pic) 
+                        VALUES (?, ?, ?)";
+        $validation_string = "sss";
+        $params = [$rest_name, $rest_desc, $rest_pic];
+        $result = $this->query($query_string, $validation_string, $params);
+        return $result["affected_rows"];
     }
     public function getOneRestaurant()
     {
