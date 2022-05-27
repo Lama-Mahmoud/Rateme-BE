47111513a11f3690af049f675308cee44b27b962
Hisham-Mohsen <mohsenhisham2@gmail.com>
2022-05-28 02:13:30 +0300

    Creating getOneRestaurant method in Restaurant class

--- Restaurant.php
+++ Restaurant.php
@@ -15,8 +15,15 @@ class Restaurant extends Database
         $result = $this->query($query_string, $validation_string, $params);
         return $result["affected_rows"];
     }
-    public function getOneRestaurant()
+    public function getOneRestaurant($rest_id)
     {
+        $query_string = "SELECT rest_name, rest_desc, rest_pic
+                        FROM $this->table
+                        WHERE rest_id = ?";
+        $validation_string = "i";
+        $params = [$rest_id];
+        $result = $this->getRow($query_string, $validation_string, $params);
+        return $result;
     }
     public function getRestaurants()
     {
