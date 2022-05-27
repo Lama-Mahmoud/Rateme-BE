d6358f2bd038584709d218aa5009a2df02220cbd
Hisham-Mohsen <mohsenhisham2@gmail.com>
2022-05-28 00:28:25 +0300

    Creating createUser method in User.php - Creating createUser API endpoint

--- User.php
+++ User.php
@@ -3,9 +3,20 @@ include_once("Database.php");
 
 class User extends Database
 {
-    public function createUser()
+    private $table = "users";
+
+    public function createUser($email, $first_name, $last_name, $dob, $password, $profile_pic, $gender)
     {
+        $hashed_password = hash("sha256", $password);
+        $query_string = "INSERT INTO $this->table 
+        (email, first_name, last_name, dob, pswd_hash, profile_pic, gender) 
+        VALUES (?, ?, ?, ?, ?, ?, ?)";
+        $validation_string = "ssssssi";
+        $params = [$email, $first_name, $last_name, $dob, $hashed_password, $profile_pic, $gender];
+        $result = $this->query($query_string, $validation_string, $params);
+        return $result["affected_rows"];
     }
+
     public function getUsers()
     {
     }
--- index.php
+++ index.php
@@ -1,11 +1,35 @@
 <?php
-include_once("Database.php");
-$db = new Database();
+include_once("User.php");
+
 
 // echo $_SERVER['REQUEST_URI'];
 // $_SERVER['REQUEST_METHOD'] === 'POST'
 
 // create user/sign up -- POST
+// print("<pre>" . print_r($_POST, true) . "</pre>");
+if ($_GET["action"] == "createUser" && $_SERVER["REQUEST_METHOD"] === "POST") {
+
+    $email = $_POST["email"];
+    $first_name = $_POST["first_name"];
+    $last_name = $_POST["last_name"];
+    $dob = $_POST["dob"];
+    $password = $_POST["password"];
+    $profile_pic = $_POST["profile_pic"];
+    $gender = $_POST["gender"];
+
+    $user = new User();
+    $affected_rows = $user->createUser(
+        $email,
+        $first_name,
+        $last_name,
+        $dob,
+        $password,
+        $profile_pic,
+        $gender
+    );
+    echo $affected_rows;
+}
+
 // login user -- POST
 // get user -- GET
 // get users -- GET
