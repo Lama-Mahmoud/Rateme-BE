230242c41ab2b4fd72c8156de5033805e64eafac
Hisham-Mohsen <mohsenhisham2@gmail.com>
2022-05-28 03:25:25 +0300

    Creating the createReview method in Review class

--- Review.php
+++ Review.php
@@ -5,8 +5,15 @@ class Review extends Database
 {
     private $table = "reviews";
 
-    public function createReview()
+    public function createReview($user_id, $rest_id, $review_content, $rate, $status = 2)
     {
+        $query_string = "INSERT INTO $this->table 
+                        (user_id, rest_id, review_content, rate, status) 
+                        VALUES (?, ?, ?, ?, ?)";
+        $validation_string = "iisii";
+        $params = [$user_id, $rest_id, $review_content, $rate, $status];
+        $result = $this->query($query_string, $validation_string, $params);
+        return $result["affected_rows"];
     }
     public function updateReviewStatus()
     {
